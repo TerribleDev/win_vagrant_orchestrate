ef391b258a32f9ffcec494a1ab6131af795be2c3
Tommy Parnell <tparnell@vistaprint.com>
2016-07-13 13:34:27 -0400

    no need to install plugin

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,19 +7,7 @@ class win_vagrant_orchestrate {
 		ensure => present,
 		mode => '0777',
 		source => 'puppet:///modules/win_vagrant_orchestrate/libcurl.dll'
-		}->
-	exec{'install-vagrant-managed-server':
- 		command  => 'C:\\HashiCorp\\Vagrant\\bin\\vagrant.exe plugin install vagrant-managed-servers --plugin-version 0.6.0',
- 		timeout  => 1800
-	}->
-	exec{'install-vagrant-winrm':
-		command  => 'C:\\HashiCorp\\Vagrant\\bin\\vagrant.exe plugin install vagrant-winrm-s',
-		timeout  => 1800
-	}->
-	exec{'install-vagrant-orchestrate':
- 		command  => 'C:\\HashiCorp\\Vagrant\\bin\\vagrant.exe plugin install vagrant-orchestrate',
- 		timeout  => 1800
-	}
+		}
   package{'mls-software-openssh':
 	ensure => 'latest',
 	provider => 'chocolatey'
